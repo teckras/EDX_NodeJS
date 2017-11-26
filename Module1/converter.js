14f08fbe46049f91a51a9dd889d4e085cdc9f530
teckras <samy.iralour@gmail.com>
2017-11-26 12:08:25 +0000

    using simple array rather than object

--- Module1/converter.js
+++ Module1/converter.js
@@ -8,17 +8,15 @@ const convertFile = () =>{
     console.log("Reading file at " + filepath)
     const data = fs.readFileSync(filepath)
     console.log("Finished reading file")
-    const buff = {
-        items: []
-    };
+    let buff = []
     csv({noheader:false})
     .fromString(data)
     .on('json',(json)=>{ //this func will be called 3 times
-        buff.items =  buff.items.concat(json)  
+        buff.push(json)  
     })
     .on('done',()=>{
         console.log('end')
-        fs.writeFileSync(path.join(__dirname, "customer-data.json"), JSON.stringify(buff.items,null,2).replace(/\n/g, '\r\n'))
+        fs.writeFileSync(path.join(__dirname, "customer-data.json"), JSON.stringify(buff,null,2).replace(/\n/g, '\r\n'))
     })
 }
 
